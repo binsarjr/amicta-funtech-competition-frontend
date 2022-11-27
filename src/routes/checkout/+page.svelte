6ae133dc96f8f3998151d4e11e44e40bfd8f7354
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-11-27 19:01:51 +0700

    feat: checkout

---
+++ src/routes/checkout/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
+	import { emailLogged, logged } from '$lib/store/preferences';
+
+	$: if (browser) {
+		$logged = false;
+		$emailLogged = '';
+		goto('/');
+	}
+</script>
