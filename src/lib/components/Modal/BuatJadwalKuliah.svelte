bfde3ac36922a9428a29eb3c10d45d77c5b2caaa
Binsar Dwi Jasuma <binsarjr121@gmail.com>
2022-11-29 15:56:13 +0700

    fix(test) form-day

--- src/lib/components/Modal/BuatJadwalKuliah.svelte
+++ src/lib/components/Modal/BuatJadwalKuliah.svelte
@@ -6,11 +6,20 @@
 	import { emailLogged } from '../../store/preferences';
 	import { jadwal } from '../../store/jadwal';
 	import type { Day } from '../../../types';
+	import { browser } from '$app/environment';
+	import { clickOutside } from '../../actions/clickOutSide';
 	// provided by Modals
 	export let isOpen = false;
 
 	let matkul = '';
 	let hari: string;
+	let dayFocus = false;
+
+	const days = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'];
+	$: filteredDay = days.filter((day) => {
+		if (!hari) return true;
+		return day.toLowerCase().includes(hari.toLowerCase());
+	});
 
 	const onSubmit = async () => {
 		if (matkul && hari) {
@@ -41,8 +50,39 @@
 			/>
 		</div>
 		<div>
-			<label for="hari" class="font-medium mb-[6px]">Pilih Hari</label>
-			<select
+			<label for="hari-asdasd" class="font-medium mb-[6px]">Pilih Hari</label>
+			<div class="relative">
+				<input
+					type="text"
+					bind:value={hari}
+					id="hari-asdasd"
+					data-cy="form-day"
+					class="w-full py-[12px] px-[15px] border outline-1 outline-[#D9019C]"
+					placeholder="Pilih Hari"
+					on:focusin={() => (dayFocus = true)}
+					on:input={() => (dayFocus = true)}
+					on:focusout={() => setTimeout(() => (dayFocus = false), 500)}
+				/>
+				{#if dayFocus}
+					<ul class="bg-white w-full absolute mt-2 rounded border" style="z-index: 2;">
+						{#each filteredDay as item}
+							<!-- svelte-ignore a11y-click-events-have-key-events -->
+							<li
+								class="w-full py-[12px] px-[15px] hover:bg-[#D9019C] hover:bg-opacity-25 cursor-pointer"
+								class:bg-[#D9019C]={item == hari}
+								class:bg-opacity-25={item == hari}
+								on:click={() => {
+									hari = item;
+									dayFocus = false;
+								}}
+							>
+								{item}
+							</li>
+						{/each}
+					</ul>
+				{/if}
+			</div>
+			<!-- <select
 				bind:value={hari}
 				data-cy="form-day"
 				id="hari"
@@ -54,7 +94,7 @@
 				{#each ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat'] as item}
 					<option value={dayInToEn(item)}>{item}</option>
 				{/each}
-			</select>
+			</select> -->
 		</div>
 	</div>
 	<!-- Footer -->
